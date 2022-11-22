f2fed267e0c8c0ea073dd42fc7b1b4306d1761f0
Akhil102 <akhileshsanodiya434@gmail.com>
2022-11-22 23:02:04 +0530

    added new code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+//css import
 import './App.css';
+import axios from 'axios';
+//route import
+import { Routes,Route } from 'react-router-dom';
+//slick css import
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+//pages import
+import HomePage from './Pages/HomePage';
+import MoviePage from './Pages/MoviePage';
+import PlayPage from './Pages/PlayPage';
+axios.defaults.baseURL ="https://api.themoviedb.org/3";
+axios.defaults.params={};
+axios.defaults.params["api_key"]= process.env.REACT_APP_API_KEY;
 
+//main function
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Routes>
+    <Route path='/' element={<HomePage/>} />
+    <Route path='/movie/:id' element={<MoviePage/>}/>
+    <Route path='/play' element={<PlayPage/>}/>
+
+   </Routes>
   );
 }
 
